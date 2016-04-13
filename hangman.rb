41b631954ebf65974c931e84aa2cef1f0c678bf2
Sayeed Siddiqui <sasidiki@gmail.com>
2016-04-12 21:54:19 -0400

    game now working

--- hangman.rb
+++ hangman.rb
@@ -1,13 +1,22 @@
+require 'set'
+
 dict = Set.new
 
-File.foreach('dictionary.txt') {|word| dict.add(word)}
-guesses = 10
+File.foreach('dictionary.txt') {|word| dict.add(word.chomp)}
+guesses = 18
 word = rand(4..7).times.map {false}
-dict.filter {|w| w.length == word.length}
+dict.reject! {|w| w.length != word.length}
 
-while guesses > 0 and dict.size > 1
-    puts word
-    pattern, dict = *dict.classify {|word| word.split(//).map {|l| l == gets.chomp}}.max_by {|p, set| set.size}
-    word = word.zip(pattern).map {|a, b| (a or b)? a : false}
-    gets -= 1
-puts word
+while guesses > 0 do
+    puts "\n"
+    puts word.map {|letter| if letter then letter else "_" end}.join " "
+    puts "Size: #{dict.size} Guesses left: #{guesses}"
+    puts "Enter guess: "
+
+    guess = gets.chomp
+    pattern, dict = *dict.classify {|word| word.split(//).map {|l| l == guess}}.max_by {|p, set| set.size}
+    word = word.zip(pattern).map {|a, b| (not a and b)? guess : a}
+    guesses -= 1
+end
+
+dict.each {|w| puts w}
