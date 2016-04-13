b5297bd9b557f6b27a7cd363e5ae6e84e98379c6
Sayeed Siddiqui <sasidiki@gmail.com>
2016-04-12 20:56:22 -0400

    simplified code into one function

--- hangman.rb
+++ hangman.rb
@@ -1,20 +1,13 @@
 dict = Set.new
 
-def pickLargest(guess)
-    pair = dict.classify {|word| word.split(//).map {|l| l == letter}}.max_by {|p, set| set.size}
-    dict = pair[1]
-    pair[o]
-end
+File.foreach('dictionary.txt') {|word| dict.add(word)}
+guesses = 10
+word = rand(4..7).times.map {false}
+dict.filter {|w| w.length == word.length}
 
-def play() 
-    File.foreach('dictionary.txt') {|word| dict.add(word)}
-    guesses = 10
-    word = rand(4..7).times.map {false}
-    dict.filter
-    while guesses > 0 and dict.size > 1
-        puts word
-        pattern = pickLargest(gets.chomp)
-        word = word.zip(pattern).map {|a, b| (a or b)? a : false}
-        gets -= 1
+while guesses > 0 and dict.size > 1
     puts word
-end
+    pattern, dict = *dict.classify {|word| word.split(//).map {|l| l == gets.chomp}}.max_by {|p, set| set.size}
+    word = word.zip(pattern).map {|a, b| (a or b)? a : false}
+    gets -= 1
+puts word
