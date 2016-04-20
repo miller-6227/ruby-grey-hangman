1576939c3986c4d948bc5694d6c2215c5c42ebd5
Sayeed Siddiqui <sasidiki@gmail.com>
2016-04-20 19:24:24 -0400

    split into methods

--- hangman.rb
+++ hangman.rb
@@ -1,22 +1,49 @@
 require 'set'
 
-dict = Set.new
+class Game
+  include ActiveModel::AttributeMethods, ActiveModel::Serializers::JSON
 
-File.foreach('dictionary.txt') {|word| dict.add(word.chomp)}
-guesses = 18
-word = rand(4..7).times.map {false}
-dict.reject! {|w| w.length != word.length}
-
-while guesses > 0 do
-    puts "\n"
-    puts word.map {|letter| if letter then letter else "_" end}.join " "
-    puts "Size: #{dict.size} Guesses left: #{guesses}"
-    puts "Enter guess: "
-
-    guess = gets.chomp
-    pattern, dict = *dict.classify {|word| word.split(//).map {|l| l == guess}}.max_by {|p, set| set.size}
-    word = word.zip(pattern).map {|a, b| (not a and b)? guess : a}
-    guesses -= 1
+  class GameOverError < StandardError; end
+      
+    def initialize
+        @letters = ("a".."z").to_a
+        @guesses = 18
+    
+        # Input all words into the dictionary
+        @dict = Set.new
+        File.foreach('dictionary.txt') {|w| @dict.add w.chomp}
+        
+        # Pick a word length and limit the dictionary to words of that length
+        @word = rand(4..8).times.map {false}
+        @dict.reject! {|w| w.length != @word.length}
+    end
+    
+    def guessed?
+        @word.count false == 0
+    end
+    
+    def finished?
+        guessed? or guesses <= 0
+    end
+    
+    def input(guess)
+        if letters.include? guess then
+            guess = gets.chomp
+            letters.delete guess
+            pattern, dict = *dict.classify {|word| word.split(//).map {|l| l == guess}}.max_by {|p, set| set.size}
+            word = word.zip(pattern).map {|a, b| (not a and b)? guess : a}
+            guesses -= 1
+        else
+            puts "error: letter not found"
+        end
+    end
+    
+    def show
+        puts "\n"
+        puts word.map {|letter| if letter then letter else "_" end}.join " "
+        puts "Size: #{dict.size} Guesses left: #{guesses}"
+        puts "Used letters: #{letters.join " "}"
+        puts "Enter guess: "
+    end
+    
 end
-
-dict.each {|w| puts w}
