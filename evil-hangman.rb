2ced22b3a5dfdbaff2cdf425420e1a95a50d6109
kmccleese <mccleese.18@osu.edu>
2016-04-21 16:15:20 -0400

    created original evil game

---
+++ evil-hangman.rb
@@ -0,0 +1,22 @@
+require 'set'
+
+dict = Set.new
+
+File.foreach('dictionary.txt') {|word| dict.add(word.chomp)}
+guesses = 18
+word = rand(4..7).times.map {false}
+dict.reject! {|w| w.length != word.length}
+
+while guesses > 0 do
+    puts "\n"
+    puts word.map {|letter| if letter then letter else "_" end}.join " "
+    puts "Size: #{dict.size} Guesses left: #{guesses}"
+    puts "Enter guess: "
+
+    guess = gets.chomp
+    pattern, dict = *dict.classify {|word| word.split(//).map {|l| l == guess}}.max_by {|p, set| set.size}
+    word = word.zip(pattern).map {|a, b| (not a and b)? guess : a}
+    guesses -= 1
+end
+
+dict.each {|w| puts w}
